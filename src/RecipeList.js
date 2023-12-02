40246daa14bb04cb411905ac61643ea6d1058b92
Sophia Nguyen <nguyencsg@gmail.com>
2023-12-01 20:09:16 -0600

    added predfined scrollbar to ingredients and preparation

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -27,8 +27,8 @@ function RecipeList({recipes, deleteRecipe}) {
               <td>{recipe.cuisine}</td>
               <td>
                 <img src={recipe.photo} alt={recipe.name}/></td>
-              <td>{recipe.ingredients}</td>
-              <td>{recipe.preparation}</td>
+              <td className="content_td"><p>{recipe.ingredients}</p></td>
+              <td className="content_td"><p>{recipe.preparation}</p></td>
               <td>
                 <button name="delete" onClick={() => deleteRecipe(recipe.id)}>Delete</button>
               </td>
